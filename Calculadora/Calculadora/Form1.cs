120a0ebb2c83d4860527845e15488b08483f9697
Leonardo Abaque <Leonardo Abaque>
2017-03-27 08:40:51 -0300

    Refatoração do código dos cálculos

--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -28,37 +28,41 @@ namespace Calculadora
 
         private void btnMais_Click(object sender, EventArgs e)
         {
-            try
-            {
-                double result = Convert.ToDouble(txtNum1.Text) + Convert.ToDouble(txtNum2.Text);
-                lblResult.Text = result.ToString();
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Digite valor numérico", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-
+            Calculo(Operador.Mais);
         }
 
         private void btnMenos_Click(object sender, EventArgs e)
         {
-            try
-            {
-                double result = Convert.ToDouble(txtNum1.Text) - Convert.ToDouble(txtNum2.Text);
-                lblResult.Text = result.ToString();
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Digite valor numérico", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            Calculo(Operador.Menos);
         }
 
         private void btnVezes_Click(object sender, EventArgs e)
         {
+            Calculo(Operador.Vezes);
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            Calculo(Operador.Dividir);   
+        }
+
+        public void Calculo(Operador operador)
+        {
             try
             {
-                double result = Convert.ToDouble(txtNum1.Text) * Convert.ToDouble(txtNum2.Text);
+                var num1 = Convert.ToDouble(txtNum1.Text);
+                var num2 = Convert.ToDouble(txtNum2.Text);
+                double result = 0;
+
+                if (operador == Operador.Mais)
+                    result = num1 + num2;
+                else if (operador == Operador.Menos)
+                    result = num1 - num2;
+                else if (operador == Operador.Vezes)
+                    result = num1 * num2;
+                else
+                    result = num1 / num2;
+
                 lblResult.Text = result.ToString();
             }
             catch (Exception)
@@ -66,17 +70,10 @@ namespace Calculadora
                 MessageBox.Show("Digite valor numérico", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+    }
 
-        private void btnDividir_Click(object sender, EventArgs e)
-        {
-            try { 
-            double result = Convert.ToDouble(txtNum1.Text) / Convert.ToDouble(txtNum2.Text);
-            lblResult.Text = result.ToString();
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Digite valor numérico", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+    public enum Operador
+    {
+        Mais, Menos, Vezes, Dividir
     }
 }
